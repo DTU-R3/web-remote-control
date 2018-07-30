d37922ac441a3700f277eddf000a3349dc7607ae
Tomasz Firynowicz <t.firynowicz@gmail.com>
2018-07-30 16:39:43 +0200

    Add primitive button based control interface

--- client_user/user.js
+++ client_user/user.js
@@ -2,6 +2,10 @@
 "use strict";
 
 var callBtn = document.querySelector('#call');
+var forwardBtn = document.querySelector('#forward');
+var stopBtn = document.querySelector('#stop');
+var turnLeftBtn = document.querySelector('#turn_left');
+var turnRightBtn = document.querySelector('#turn_right');
 
 callBtn.addEventListener("click", event => {
   var call = peer.call("robot", new MediaStream());
@@ -30,4 +34,103 @@ peer.on('error', err => {
 
 peer.on('open', id => {
   console.log('My peer ID is: ' + id);
+});
+
+
+// robot control
+let rosbridge_url = 'ws://pi-desktop:9090/';
+
+var ros = new ROSLIB.Ros({
+  url : rosbridge_url
+});
+
+ros.on('connection', function() {
+  console.log('Connected to ', rosbridge_url);
+});
+
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
+
+var cmd_vel = new ROSLIB.Topic({
+  ros : ros,
+  name : '/cmd_vel',
+  messageType : 'geometry_msgs/Twist'
+});
+
+var forwardMsg = new ROSLIB.Message({
+  "linear": {
+    "x": 1,
+    "y": 0,
+    "z": 0
+  },
+  "angular": {
+    "x": 0,
+    "y": 0,
+    "z": 0
+  }
+});
+
+var stopMsg = new ROSLIB.Message({
+  "linear": {
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
+  "angular": {
+    "x": 0,
+    "y": 0,
+    "z": 0
+  }
+});
+
+var turnLeftMsg = new ROSLIB.Message({
+  "linear": {
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
+  "angular": {
+    "x": 0,
+    "y": 0,
+    "z": 1
+  }
+});
+
+var turnRightMsg = new ROSLIB.Message({
+  "linear": {
+    "x": 0,
+    "y": 0,
+    "z": 0
+  },
+  "angular": {
+    "x": 0,
+    "y": 0,
+    "z": -1
+  }
+});
+
+
+// keyboard robot control
+window.addEventListener('keypress', function (e) {
+    if (e.code == "ArrowUp") {
+        alert("up");
+    }
+}, false);
+
+// button robot control
+forwardBtn.addEventListener("click", event => {
+  cmd_vel.publish(forwardMsg);
+});
+
+stopBtn.addEventListener("click", event => {
+  cmd_vel.publish(stopMsg);
+});
+
+turnLeftBtn.addEventListener("click", event => {
+  cmd_vel.publish(turnLeftMsg);
+});
+
+turnRightBtn.addEventListener("click", event => {
+  cmd_vel.publish(turnRightMsg);
 });
