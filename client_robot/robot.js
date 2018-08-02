23f9e650097b5eff6054986cb7807fd7f2ebde59
Tomasz Firynowicz <t.firynowicz@gmail.com>
2018-08-02 19:55:24 +0200

    Add connection UI

--- client_robot/robot.js
+++ client_robot/robot.js
@@ -1,36 +1,39 @@
 /* jshint esversion: 6 */
 "use strict";
 
-// video and audio access
-var mediaStream;
+var loginBtn = document.querySelector('#login');
+var loginIn = document.querySelector('#robotID');
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
 .then(function(stream) {
-  // connecting to peerJS signaling server
-  let peer = new Peer('robot', {
-    debug: 3,
-    host: 'r3.man.dtu.dk',
-    path: '/ws',
-    port: 443,
-    secure: true,
-    config: {
-      'iceServers': [{
-        urls: 'stun:stun.l.google.com:19302' // free STUN server from google
-      }]
-    }
-  });
+  loginBtn.addEventListener("click", event => {
+    // connecting to peerJS signaling server
+    let peer = new Peer(loginIn.value, {
+      debug: 3,
+      host: 'r3.man.dtu.dk',
+      path: '/ws',
+      port: 443,
+      secure: true,
+      config: {
+        'iceServers': [{
+          urls: 'stun:stun.l.google.com:19302' // free STUN server from google
+        }]
+      }
+    });
 
+    peer.on('error', err => {
+      alert(err);
+      console.log(err);
+    });
 
-  peer.on('error', err => {
-    console.log(err);
-  });
+    peer.on('open', id => {
+      alert('Sucessfully logged in with ID ' + id);
+      console.log('My peer ID is: ' + id);
+    });
 
-  peer.on('open', id => {
-    console.log('My peer ID is: ' + id);
-  });
-
-  peer.on('call', call => {
-    call.answer(stream);
+    peer.on('call', call => {
+      call.answer(stream);
+    });
   });
 })
 .catch(function(err) {
--- client_user/user.js
+++ client_user/user.js
@@ -2,16 +2,37 @@
 "use strict";
 
 var callBtn = document.querySelector('#call');
-var forwardBtn = document.querySelector('#forward');
-var stopBtn = document.querySelector('#stop');
-var turnLeftBtn = document.querySelector('#turn_left');
-var turnRightBtn = document.querySelector('#turn_right');
+var connectBtn = document.querySelector('#connect');
 
-callBtn.addEventListener("click", event => {
-  var call = peer.call("robot", new MediaStream());
+callBtn.addEventListener('click', event => {
+
+  var call = peer.call(document.querySelector('#robotID').value, new MediaStream());
 
   call.on('stream', stream => {
     document.querySelector('#player').srcObject = stream;
+  });
+});
+
+connectBtn.addEventListener('click', event => {
+
+  var ros = new ROSLIB.Ros({
+    url : document.querySelector('#robotAddress').value
+  });
+
+  ros.on('connection', function() {
+    alert('Connected to ' + document.querySelector('#robotAddress').value);
+    console.log('Connected to ', document.querySelector('#robotAddress').value);
+
+    // add keyboard controls
+    var teleop = new KEYBOARDTELEOP.Teleop({
+      ros: ros,
+      topic: '/cmd_vel'
+    });
+  });
+
+  ros.on('error', function(error) {
+    alert(error);
+    console.log(error);
   });
 });
 
@@ -29,110 +50,10 @@ let peer = new Peer('user' + Math.random().toString(36).substr(2, 5), {
 });
 
 peer.on('error', err => {
+  alert(err);
   console.log(err);
 });
 
 peer.on('open', id => {
   console.log('My peer ID is: ' + id);
-});
-
-
-// robot control
-let rosbridge_url =
-  // 'ws://raspi-ros00:9090/';
-  'ws://pi-desktop:9090/';
-  // 'ws://192.38.90.68:9090/';
-
-var ros = new ROSLIB.Ros({
-  url : rosbridge_url
-});
-
-
-ros.on('connection', function() {
-  console.log('Connected to ', rosbridge_url);
-
-  var teleop = new KEYBOARDTELEOP.Teleop({
-    ros: ros,
-    topic: '/cmd_vel'
-  });
-
-  // var cmd_vel = new ROSLIB.Topic({
-  //   ros : ros,
-  //   name : '/cmd_vel',
-  //   messageType : 'geometry_msgs/Twist'
-  // });
-  //
-  // var forwardMsg = new ROSLIB.Message({
-  //   "linear": {
-  //     "x": 1,
-  //     "y": 0,
-  //     "z": 0
-  //   },
-  //   "angular": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 0
-  //   }
-  // });
-  //
-  // var stopMsg = new ROSLIB.Message({
-  //   "linear": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 0
-  //   },
-  //   "angular": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 0
-  //   }
-  // });
-  //
-  // var turnLeftMsg = new ROSLIB.Message({
-  //   "linear": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 0
-  //   },
-  //   "angular": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 1
-  //   }
-  // });
-  //
-  // var turnRightMsg = new ROSLIB.Message({
-  //   "linear": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": 0
-  //   },
-  //   "angular": {
-  //     "x": 0,
-  //     "y": 0,
-  //     "z": -1
-  //   }
-  // });
-  //
-  //
-  // // button robot control
-  // forwardBtn.addEventListener("click", event => {
-  //   cmd_vel.publish(forwardMsg);
-  // });
-  //
-  // stopBtn.addEventListener("click", event => {
-  //   cmd_vel.publish(stopMsg);
-  // });
-  //
-  // turnLeftBtn.addEventListener("click", event => {
-  //   cmd_vel.publish(turnLeftMsg);
-  // });
-  //
-  // turnRightBtn.addEventListener("click", event => {
-  //   cmd_vel.publish(turnRightMsg);
-  // });
-});
-
-ros.on('error', function(error) {
-  console.log('Error connecting to websocket server: ', error);
 });
