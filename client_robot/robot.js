7686cc5e89a49d5d3273721f1949a63ebb0bd9f5
Tomasz Firynowicz <t.firynowicz@gmail.com>
2018-08-02 13:46:08 +0200

    Fix Firefox compatibility

--- client_robot/robot.js
+++ client_robot/robot.js
@@ -4,35 +4,35 @@
 // video and audio access
 var mediaStream;
 
-navigator.getUserMedia({ video: true, audio: true }, stream => {
-  mediaStream = stream;
-}, error => {
-  console.log(error);
-});
-
-// connecting to peerJS signaling server
-let peer = new Peer('robot', {
-  debug: 3,
-  host: 'r3.man.dtu.dk',
-  path: '/ws',
-  port: 443,
-  secure: true,
-  config: {
-    'iceServers': [{
-      urls: 'stun:stun.l.google.com:19302' // free STUN server from google
-    }]
-  }
-});
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+.then(function(stream) {
+  // connecting to peerJS signaling server
+  let peer = new Peer('robot', {
+    debug: 3,
+    host: 'r3.man.dtu.dk',
+    path: '/ws',
+    port: 443,
+    secure: true,
+    config: {
+      'iceServers': [{
+        urls: 'stun:stun.l.google.com:19302' // free STUN server from google
+      }]
+    }
+  });
 
 
-peer.on('error', err => {
-  console.log(err);
-});
+  peer.on('error', err => {
+    console.log(err);
+  });
 
-peer.on('open', id => {
-  console.log('My peer ID is: ' + id);
-});
+  peer.on('open', id => {
+    console.log('My peer ID is: ' + id);
+  });
 
-peer.on('call', call => {
-  call.answer(mediaStream);
+  peer.on('call', call => {
+    call.answer(stream);
+  });
+})
+.catch(function(err) {
+  console.log('GetUserMedia error:', err);
 });
