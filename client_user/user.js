37a4d4bcb3c97203f87d8bcacc8cc74fc74f0b08
Tomasz Firynowicz <t.firynowicz@gmail.com>
2018-08-06 14:20:57 +0200

    Improve ROS connection logic

--- client_user/user.js
+++ client_user/user.js
@@ -3,8 +3,45 @@
 
 var callBtn = document.querySelector('#call');
 var connectBtn = document.querySelector('#connect');
+let mapDiv = document.querySelector('#map');
 
-var ros;
+let ros = new ROSLIB.Ros;
+
+let reconnect = false;
+
+ros.on('connection', () => {
+  console.log('Connected to ', document.querySelector('#robotAddress').value);
+
+  // add keyboard controls
+  var teleop = new KEYBOARDTELEOP.Teleop({
+    ros: ros,
+    topic: '/cmd_vel'
+  });
+
+  // open map
+  let mapObject = document.createElement('object');
+  mapObject.innerHTML = 'Warning: Map could not be included.';
+  mapObject.className = 'fullheight';
+  mapObject.setAttribute('id', 'mapobject');
+  mapObject.setAttribute('data', '../r3-geofencing/Ros.html');
+
+  mapDiv.appendChild(mapObject);
+});
+
+ros.on('close', () => {
+    mapDiv.innerHTML = '';
+
+    if (reconnect) {
+      ros.connect(document.querySelector('#robotAddress').value);
+      reconnect = false;
+    }
+});
+
+ros.on('error', error => {
+  alert('Can\'t connect to ' + document.querySelector('#robotAddress').value);
+  console.log('Can\'t connect to ' + document.querySelector('#robotAddress').value, error);
+});
+
 
 callBtn.addEventListener('click', event => {
 
@@ -16,43 +53,13 @@ callBtn.addEventListener('click', event => {
 });
 
 connectBtn.addEventListener('click', event => {
-
-  if (ros instanceof ROSLIB.Ros){
+  if (ros.isConnected) {
+    reconnect = true;
     ros.close();
-    console.log("closed!");
   }
-
-  ros = new ROSLIB.Ros({
-    url : document.querySelector('#robotAddress').value
-  });
-
-  ros.on('connection', function() {
-    alert('Connected to ' + document.querySelector('#robotAddress').value);
-    console.log('Connected to ', document.querySelector('#robotAddress').value);
-
-    // add keyboard controls
-    var teleop = new KEYBOARDTELEOP.Teleop({
-      ros: ros,
-      topic: '/cmd_vel'
-    });
-  });
-
-  ros.on('error', function(error) {
-    alert(error);
-    console.log(error);
-  });
-
-  // reload map
-  let mapDiv = document.querySelector('#map');
-  mapDiv.innerHTML = ''; // clears mapDiv - important if map was already loaded once
-
-  let mapObject = document.createElement('object');
-  mapObject.innerHTML = 'Warning: Map could not be included.';
-  mapObject.className = 'fullheight';
-  mapObject.setAttribute('id', 'mapobject');
-  mapObject.setAttribute('data', '../r3-geofencing/Ros.html');
-
-  mapDiv.appendChild(mapObject);
+  else {
+    ros.connect(document.querySelector('#robotAddress').value);
+  }
 });
 
 let peer = new Peer('user' + Math.random().toString(36).substr(2, 5), {
